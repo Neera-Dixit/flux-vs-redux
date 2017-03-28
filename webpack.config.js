8af184e261c3177420c8f052f5c56d156edb2a32
Dixit, Neeraj <Neeraj.Dixit@in.tesco.com>
2017-03-28 08:50:24 +0100

    project setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+var fs = require('fs');
+
+var clientBuildPath = path.resolve(__dirname, 'public', 'build');
+var clientMainPath = path.resolve(__dirname, 'src','components','index.js');
+
+
+ module.exports = {
+         name : 'client',
+         target : 'web',
+         entry:  [
+                    clientMainPath
+                ],
+         output: {
+             path: clientBuildPath,
+             publicPath: "public/build",
+             filename: 'bundleclient.js'
+         },
+         module: {
+             loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loaders: ['babel-loader']
+         }]
+        }
+    };
